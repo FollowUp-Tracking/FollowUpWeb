7897934a19d0119ed82ea37c5ef0c05cbd24bae3
Sergio Calderón <sergiocalde00@gmail.com>
2022-04-01 12:55:42 +0200

    login y resgiter mejorados

--- src/main/java/es/upm/dit/isst/followmeweb/controller/TrazaController.java
+++ src/main/java/es/upm/dit/isst/followmeweb/controller/TrazaController.java
@@ -1,6 +1,5 @@
 package es.upm.dit.isst.followmeweb.controller;
 
-import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,7 +7,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import es.upm.dit.isst.followmeweb.model.Traza;
@@ -16,7 +14,7 @@ import es.upm.dit.isst.followmeweb.model.Traza;
 @Controller
 public class TrazaController {
 
-    public final String TRAZAMANAGER_STRING = "http://localhost:8083/trazas";
+    public final String TRAZAMANAGER_STRING = "http://localhost:8083/trazas/";
     public static final String VISTA_REGISTER = "register";
     public static final String VISTA_LOGIN = "login";
     public static final String VISTA_HISTORICO = "historico";
