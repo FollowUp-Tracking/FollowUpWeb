15c033ee7869ea4fc22e209b162b26421b3c2aba
Sergio Calderón <sergiocalde00@gmail.com>
2022-04-01 20:11:55 +0200

    trazas bbdd

--- src/main/java/es/upm/dit/isst/followmeweb/model/Traza.java
+++ src/main/java/es/upm/dit/isst/followmeweb/model/Traza.java
@@ -7,16 +7,15 @@ import javax.validation.constraints.NotEmpty;
 public class Traza {
     
     private int id;
+    
     private double latitud;
+    
     private double longitud;
+    
     private Date fecha;
-    private int idPedido;
-    private String cliente;
-    private String vendedor;
-    private String producto;
-    private int idRepartidor;
+
     @NotEmpty
-    private int estado;
+    private int idPedido;
     
     public Traza() {
     }
@@ -60,45 +59,4 @@ public class Traza {
     public void setIdPedido(int idPedido) {
         this.idPedido = idPedido;
     }
-
-    public String getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(String cliente) {
-        this.cliente = cliente;
-    }
-
-    public String getVendedor() {
-        return vendedor;
-    }
-
-    public void setVendedor(String vendedor) {
-        this.vendedor = vendedor;
-    }
-
-    public String getProducto() {
-        return producto;
-    }
-
-    public void setProducto(String producto) {
-        this.producto = producto;
-    }
-
-    public int getIdRepartidor() {
-        return idRepartidor;
-    }
-
-    public void setIdRepartidor(int idRepartidor) {
-        this.idRepartidor = idRepartidor;
-    }
-
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
-
 }
